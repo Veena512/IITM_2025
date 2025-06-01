33803801d926329cea32ffb52694a896cc4be554
Veena <pveena2020@gmail.com>
2025-06-01 09:49:50 +0530

    Add test.py file

---
+++ test.py
@@ -0,0 +1,14 @@
+# Hello World Comment Line
+
+def add(a, b):
+  pass
+
+def subtract(a,b):
+  pass
+
+if __name__ == "__main__":
+  a = 5
+  b = 6
+
+  print(f"Add {a} and {b} to get {add(a,b)}")
+  print(f"Subtract {a} from {b} to get {subtract(a,b)}")
